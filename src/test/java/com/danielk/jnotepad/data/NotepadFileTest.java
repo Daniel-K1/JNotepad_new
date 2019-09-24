2dfa7609d94f87bdb54c2a08dc26b14a59ff74fa
Daniel-K1 <daniel@mobilny-biznes.pl>
2019-09-24 23:01:04 +0200

    Junit5 - stubs only,not fully operational yet (part3)

--- src/test/java/com/danielk/jnotepad/data/NotepadFileTest.java
+++ src/test/java/com/danielk/jnotepad/data/NotepadFileTest.java
@@ -1,22 +1,45 @@
 package com.danielk.jnotepad.data;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 class NotepadFileTest {
 
-    @Test
-    void openWithEncoding() {
+    private File selectedFile;
+    private static final Logger LOG = LoggerFactory.getLogger(NotepadFileTest.class);
 
+    @ParameterizedTest
+    @MethodSource("argumentFactory")
+    void openWithEncoding_parametrized(String charsetName) {
 
+        boolean ok = true;
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(selectedFile), charsetName));
+        } catch (IOException e) {
+            ok=false;
+            LOG.error("IO Error while opening the file in test method: " + e.getMessage());
+        }
+    }
 
-
+    private static Stream<Arguments> argumentFactory() {
+        return Stream.of(
+                Arguments.of("Cp1250"),
+                Arguments.of("UTF8"),
+                Arguments.of("UTF16"),
+                Arguments.of("ASCII"),
+                Arguments.of("ISO8859_2")
+        );
     }
 
 }
---
+++ src/test/java/com/danielk/jnotepad/gui/NotepadPromptsTest.java
@@ -0,0 +1,34 @@
+package com.danielk.jnotepad.gui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotepadPromptsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void openFile() {
+    }
+
+    @Test
+    void saveFile() {
+    }
+
+    @Test
+    void printFile() {
+    }
+
+    @Test
+    void setFont() {
+    }
+}
